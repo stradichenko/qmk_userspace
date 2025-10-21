05d39857a2cdd821d1fd8c6e460436768a85649d
stradichenko <sittoche@gmail.com>
2025-10-21 11:31:53 +0200

    full Colemak-like optimization, ZXCV fixed v2.1

--- keyboards/bastardkb/charybdis/3x6/keymaps/my-keyboard/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/my-keyboard/keymap.c
@@ -50,11 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       MT(MOD_LGUI, KC_TAB),    KC_Q,    KC_G,    KC_M,    KC_L,    KC_W,    KC_B,    KC_Y,    KC_U,    KC_V,  KC_SCLN, RAISE,
+       MT(MOD_LGUI, KC_TAB),    KC_Q,    KC_G,    KC_M,    KC_L,    KC_W,    KC_Y,    KC_F,    KC_U,    KC_B,  KC_SCLN, RAISE,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        MT(MOD_LSFT, KC_ESC),    KC_D,    KC_S,    KC_T,    KC_N,    KC_R,       KC_I,    KC_A,    KC_E,    KC_O, KC_H,    KC_RSFT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_F,    KC_J,       KC_K,    KC_P, KC_COMM,  KC_DOT, PT_SLSH, KC_RCTL,
+       KC_LCTL,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_J,       KC_K,    KC_P, KC_COMM,  KC_DOT, PT_SLSH, KC_RCTL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_BSPC,  KC_SPC,   LOWER,    KC_RGUI,  MT(MOD_RALT, KC_ENT)
   //                            ╰───────────────────────────╯ ╰──────────────────╯
